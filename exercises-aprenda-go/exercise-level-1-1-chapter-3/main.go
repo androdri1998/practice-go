24640853e6067ab5c69be729ae12f3e23a8733fb
André Rodrigues <andre.rodrigues98@outlook.com>
2023-11-13 15:15:41 -0300

    feat: add exercise 1 to level 1 chapter 3

---
+++ exercises-aprenda-go/exercise-level-1-1-chapter-3/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y, z := 42, "James Bond", true
+
+	fmt.Println(x, y, z)
+	fmt.Printf("X: %v\n", x)
+	fmt.Printf("Y: %v\n", y)
+	fmt.Printf("Z: %v\n", z)
+}
