81ea76a3aadef29404599ac852fe7815ccfb0842
André Rodrigues <andre.rodrigues98@outlook.com>
2023-11-14 19:57:12 -0300

    fix: add rune to conversion

--- exercises-aprenda-go/challenges/chapter-6-challenge-1/main.go
+++ exercises-aprenda-go/challenges/chapter-6-challenge-1/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	for index := 33; index <= 122; index++ {
-		fmt.Printf("%v\t %d\t %#x\t %#U\n", string(index), index, index, index)
+		fmt.Printf("%v\t %d\t %#x\t %#U\n", string(rune(index)), index, index, index)
 	}
 }
