a2703b92e576c3c6bd8745ee951e44ffe0e1dee0
André Rodrigues <andre.rodrigues98@outlook.com>
2023-11-16 08:02:18 -0300

    refactor: exercise 6 to level 4 chapter 9

--- exercises-aprenda-go/level-4-exercise-6-chapter-9/main.go
+++ exercises-aprenda-go/level-4-exercise-6-chapter-9/main.go
@@ -4,11 +4,7 @@ import "fmt"
 
 func main() {
 	list := make([]string, 26, 26)
-	states := []string{"Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"}
-
-	for index := 0; index < len(states); index++ {
-		list[index] = states[index]
-	}
+	list = []string{"Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"}
 
 	fmt.Println(len(list))
 	fmt.Println(cap(list))
